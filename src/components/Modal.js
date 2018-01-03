fb736292d10e7c22a9f85665db8f8ac8b9322501
YozhikM <botev.st@gmail.com>
2018-01-03 20:51:42 +0600

    feat(Modal): added component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,9 @@
 /* @flow */
 
-import React from "react";
-import s from "./style.scss";
-import { isArray } from "util";
+import React from 'react';
 
 type Props = {
+  length: number,
   match: {
     _links: Object,
     date: string,
@@ -29,20 +28,25 @@ type State = {};
 
 export default class Modal extends React.Component<Props, State> {
   getDescription = () => {
-    const { match } = this.props;
+    const { match, length } = this.props;
     const {
-      topOfTable,
       homePlace,
       homeTeamName,
       awayPlace,
       awayTeamName,
       diffInPlace,
-      diffInPoints
+      diffInPoints,
+      rating,
+      status
     } = match;
 
-    if (homePlace <= topOfTable || awayPlace <= topOfTable) {
-      return `The match can be interesting, because the teams are located at the top of the standings. ${homeTeamName} is on the ${homePlace} place, and ${awayTeamName} is on the ${awayPlace} place. The difference between them:  ${diffInPoints} points and ${diffInPlace} places`;
+    console.log(status);
+
+    if (rating <= length / 6) {
+      return `The match ${status === 'FINISHED' ? 'was' : 'can get'} hot, because the teams are close to each other and close to the highest positions. ${homeTeamName} is on the ${homePlace} place, and ${awayTeamName} is on the ${awayPlace} place. The difference between them:  ${diffInPoints} points and ${diffInPlace} places`;
     }
+
+    return `The match ${status === 'FINISHED' ? 'was' : 'can be'} interesting, because the teams are located at the top of the standings. ${homeTeamName} is on the ${homePlace} place, and ${awayTeamName} is on the ${awayPlace} place. The difference between them:  ${diffInPoints} points and ${diffInPlace} places`;
   };
 
   render() {
